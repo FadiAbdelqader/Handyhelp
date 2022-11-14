c7a6826de90f01c2078241f951d2bf5b3be35736
FadiAbdelqader <abdelqader.fadi.h@gmail.com>
2022-11-14 10:30:42 +0100

    init project bis

---
+++ src/test/java/com/miage/handyhelp/HandyhelpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miage.handyhelp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HandyhelpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
